b0681ddd10df1fc345037ea3532a61680db67c35
ViktoriiaBarma <120780368+ViktoriiaBarma@users.noreply.github.com>
2023-11-20 13:16:04 +0000

    Deploying to gh-pages from @ ViktoriiaBarma/RentalCar@e6e734b51d9de076c40af030f2e974f602524d7a 🚀

---
+++ assets/FavoritePage-86daa6bd.js
@@ -0,0 +1,8 @@
+import{n as s,u as r,j as t}from"./index-959778f3.js";import{s as a,C as n}from"./CarsList-b5097152.js";const i=s.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,o=s.h1`
+  margin: 0;
+  margin-right: 10px;
+`,m=()=>{const e=r(a);return t.jsxs(i,{children:[e.length===0&&t.jsx(o,{children:"You don`t have favorite cars"}),t.jsx(n,{cars:e})]})};export{m as default};
---
+++ assets/HomePage-72db818e.js
@@ -0,0 +1,32 @@
+import{n as o,j as t,L as n}from"./index-959778f3.js";const r="/RentalCar/assets/car-72cd2ef2.jpg",e=o.h1`
+  margin: 0;
+  margin-right: 10px;
+  margin-bottom: 32px;
+`,i=o.div`
+  width: 100%;
+  height: 100hv;
+
+  background-image: url(${r});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  padding-top: 100px;
+  padding-bottom: 500px;
+  padding-left: 50px;
+`,a=o.button`
+  border-radius: 12px;
+  border: 0.5px solid transparent;
+  padding: 12px 99.5px;
+  color: #fff;
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.3;
+  font-family: inherit;
+  background-color: #3470ff;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  &:hover {
+    background-color: #0b44cd;
+  }
+`,d=()=>t.jsxs(i,{children:[t.jsx(e,{children:"Let`s arrange an interesting trip"}),t.jsx(a,{children:t.jsx(n,{style:{textDecoration:"none",color:"white"},to:"/catalog",children:"Let`s go"})})]});export{d as default};
